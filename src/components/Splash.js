c76ab1dcbf365ba62f3db94a9e71ec1f7f410894
Abdullah Wafy <red.sulphur786@gmail.com>
2020-08-25 23:57:27 -0700

    splash page almost done

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -16,11 +16,11 @@ function Splash() {
                 <h1>Work with anybody! Whether it’s for work, a side project or even the next family vacation, Edara manages your team's logistics.</h1>
             </div>
             <div class='parallax2'></div>
-            <div id='splash-middle' class='parallax-divs'>
+            <div id='splash-middle2' class='parallax-divs'>
                 <h1>Information neatly structured! Dive into the details by adding comments, attachments, due dates, and more directly to Edara cards. Collaborate on projects from beginning to end.</h1>
             </div>
             <div class='parallax3'></div>
-            <div id='splash-middle' class='parallax-divs'>
+            <div id='splash-middle3' class='parallax-divs'>
                 <h1>Check it out! Go from idea to action in seconds with Edara’s intuitively simple moveable lists and cards.</h1>
             </div>
             <div class='parallax4'></div>
